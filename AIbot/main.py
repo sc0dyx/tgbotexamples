864dc06f226ea3f6dd6b3c241e8f58f58201dcb2
sc0dyx <profftalexandr@gmail.com>
2025-12-06 22:11:33 +0300

    Create model, add context

--- AIbot/main.py
+++ AIbot/main.py
@@ -1,11 +1,28 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
-
-model_id = "deepseek-ai/deepseek-vl2-tiny"
-tokenizer = AutoTokenizer.from_pretrained(model_id, trust_remote_code=True)
-model = AutoModelForCausalLM.from_pretrained(model_id, trust_remote_code=True)
-
-inputs = tokenizer(
-    "Привет! Напиши короткий рассказ про программиста.", return_tensors="pt"
+import asyncio
+import re
+from aiogram import Bot, Dispatcher, F
+from aiogram.types import (
+    Message,
 )
-outputs = model.generate(**inputs, max_length=100)
-print(tokenizer.decode(outputs[0], skip_special_tokens=True))
+from config import TOKEN
+from model import model
+from aiogram.enums import ChatAction, ParseMode
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+
+@dp.message(F.text)
+async def handle_message(message: Message):
+    await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
+
+    answer = model(message.text, message.chat.id)
+    await message.answer(answer)
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
